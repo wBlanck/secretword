123500471688e57d8ec5f1c0baf3b86a9cdf109d
wBlanck <blanck.william@gmail.com>
2022-03-25 11:43:34 +0100

    test and code for non zero guessed words

--- src/components/GuessedWords.test.js
+++ src/components/GuessedWords.test.js
@@ -32,4 +32,29 @@ describe("if there are no words guessed", () => {
     expect(instructions.text().length).not.toBe(0);
   });
 });
-describe("if there are words guessed", () => {});
+describe("if there are words guessed", () => {
+  let wrapper;
+
+  const guessedWords = [
+    { guessedWord: "train", letterMatchCount: 2 },
+    { guessedWord: "pieeru", letterMatchCount: 4 },
+    { guessedWord: "pipers", letterMatchCount: 5 },
+  ];
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+
+  test("renders without error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+  test("renders 'guessed words' section", () => {
+    const guessedWordsNode = findByTestAttr(wrapper, "guessed-words");
+    expect(guessedWordsNode.length).toBe(1);
+  });
+  test("correct number of guessed words", () => {
+    const guessedWordsNodes = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWordsNodes.length).toBe(guessedWords.length);
+  });
+});
